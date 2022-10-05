21a6f8789a6bc4a19ab80b7da644bb90dbd8a3c4
Sierra Lopez <sierralopez1006@gmail.com>
2022-10-04 21:44:36 -0700

    trying to fetch data but idk what to fetch because the activity is extremely vague

--- public/index.js
+++ public/index.js
@@ -1,9 +1,31 @@
 async function main() {
-
+    let response = await fetch('https://api.twelvedata.com/stocks');
     const timeChartCanvas = document.querySelector('#time-chart');
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
+    let result = await response.json();
+    console.log(result);
 
+
+    new Chart(timeChartCanvas.getContext('2d'), {
+        type: 'line',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor:  'rgba(255, 99, 132, 0.2)',
+                borderColor: 'rgba(255, 99, 132, 1)'
+            }]
+        }
+    });
+    
+    let GME = result.GME
+    let MSFT = result.MSFT
+    let DIS = result.DIS
+    let BTNX = result.BTNX
+
+    const stocks = [GME, MSFT, DIS, BTNX];
 }
-
 main()
+//api key 4f0046390dff441d97cede907e0c6916
